b37155a4bffb8af15d060b5a5469fb70c203e649
AffanMohammed1 <affankmmohammed@gmail.com>
2025-04-20 16:07:04 -0400

    Docstrings for cli.py

--- cli.py
+++ cli.py
@@ -3,6 +3,7 @@ from parser import load_players
 from optimizer import greedy_team
 
 def main():
+    """This function returns the best possible combination for the squad based on the budget."""
     p = argparse.ArgumentParser("DraftGenius: Premier League Draft Assistant")
     p.add_argument('--budget', type=float, required=True, help="Total salary cap for your 11‑player squad")
     p.add_argument('--size',  type=int, default=11, help="Squad size (default: 11)")
@@ -14,8 +15,10 @@ def main():
 
     print("\nRecommended Squad:")
     for pl in team.players:
+        """This for loop returns the best possible squad combination for the user."""
         print(" -", pl)
-    print(f"\nTotal Cost: {team.total_cost():.2f}, Total Value: {team.total_value():.2f}")
+        print(f"\nTotal Cost: {team.total_cost():.2f}, Total Value: {team.total_value():.2f}")
 
 if __name__ == "__main__":
+    """The main function."""
     main()
